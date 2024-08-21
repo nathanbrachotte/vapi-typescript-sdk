1d115c989341c15e9d2f89897964cd43a8318de8
N8 <nathan.brachotte+github@gmail.com>
2024-08-21 20:49:09 +0200

    Format code with Prettier

---
+++ dist/utils.d.ts
@@ -0,0 +1,2 @@
+export declare const add: (a: number, b: number) => number;
+//# sourceMappingURL=utils.d.ts.map
---
+++ dist/utils.js
@@ -0,0 +1,2 @@
+export const add = (a, b) => a + b;
+//# sourceMappingURL=utils.js.map
